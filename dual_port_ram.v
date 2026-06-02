a7e51791b2905ce997d6dc7f031a6cab883889db
Arin Saraf <arinsaraf2006@gmail.com>
2026-06-02 22:39:02 +0530

    to calc 4 point fft
    Changed parameter N from 8 to 4 and updated memory initialization accordingly.

--- dual_port_ram.v
+++ dual_port_ram.v
@@ -10,13 +10,23 @@ module dual_port_ram ( clk,
                        data_out_B);
 
     input clk,we,rst;                      
-    parameter N=8; 
+    parameter N=4; 
     input[$clog2(N)-1:0] addra,addrb;
     input signed [31:0] data_in_A,data_in_B;    
     output reg signed [31:0] data_out_A,data_out_B;
-    
+   
     reg signed [31:0] mem [0:N-1]; /*since we have to fetch both the complex and 
-    real part we have used 32 bit, 16 bit for complex 16 bit for signed*/
+    real numbers we have used 32 bit, 16 bit for complex 16 bit for signed*/
+    
+    
+    initial begin
+        
+        mem[0] = 32'h4000_0000;
+        mem[1] = 32'h1000_0000;
+        mem[2] = 32'h2000_0000;
+        mem[3] = 32'h0000_0000;
+    
+    end
     
     always @(posedge clk or negedge rst) begin
         if(!rst) begin
