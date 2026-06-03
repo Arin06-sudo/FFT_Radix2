b2bc23df80950b66b6ef5dd8e8af50516651cf4a
Arin Saraf <arinsaraf2006@gmail.com>
2026-06-04 01:00:58 +0530

    final complex multiplier

--- complex_multiplier.v
+++ complex_multiplier.v
@@ -1,10 +1,10 @@
 
 module cmultiplier(input signed [15:0] real1,complex1,real2,complex2,
                    input clk,rst,
-                   output reg signed [15:0] real_out,complex_out);
+                   output reg signed  [15:0] real_out,complex_out);
                    
-                   reg[31:0] r1r2,c1c2,r1c2,r2c1;
-                   wire[31:0] sum_real,sum_complex;
+                   reg  signed [31:0] r1r2,c1c2,r1c2,r2c1;
+                   wire signed [31:0] sum_real,sum_complex;
                    
                    always@(posedge clk or negedge rst)begin
                    
@@ -28,13 +28,15 @@ module cmultiplier(input signed [15:0] real1,complex1,real2,complex2,
                    
                    
                    always@(posedge clk or negedge rst) begin
-                    if(rst) begin
-                        real_out <= 32'd0;
-                        complex_out <= 32'd0;
+                    if(!rst) begin
+                        real_out <= 16'd0;
+                        complex_out <= 16'd0;
                     end
                     
                     else begin
-                        real_out <= sum_real[30:15];
+                        real_out <= sum_real[30:15]; /*after multiplication, the format is Q2.30, so to 
+                        convert it to Q1.15, we have to perform truncation of the final multiplication result*/
+                        
                         complex_out <= sum_complex[30:15];
                     end
                    end
